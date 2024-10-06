09d9f46a0e42ac10669312e9ba39261fcc0c5258
Ushkamyob <nick.ch2018@bk.ru>
2024-10-06 21:14:59 +0300

    add ActivityB, realise "Open ActivityB" button

--- app/src/main/java/com/nuclan2024/hw2/Activity_A.kt
+++ app/src/main/java/com/nuclan2024/hw2/Activity_A.kt
@@ -1,5 +1,6 @@
 package com.nuclan2024.hw2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -10,9 +11,18 @@ class Activity_A : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_a)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
         val openActivityB = findViewById<Button>(R.id.open_activity_b)
         openActivityB.setOnClickListener {
+            val intent = Intent(this, Activity_B::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
 
+            startActivity(intent)
         }
         val openFragmentB = findViewById<Button>(R.id.open_fragment_b)
         openFragmentB.setOnClickListener {
